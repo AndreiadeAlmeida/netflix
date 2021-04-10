ce5966f481acd27d3ff5a5484649b5251feb1b9c
Andreia <andreia.ca22@gmail.com>
2021-04-10 15:35:58 +0100

    netflix - opt form

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Jumbotron } from './jumbotron/jumbotron';
 export { default as Accordion } from './accordion/accordion';
 export { default as Footer } from './footer/footer'
+export { default as OptForm } from './optForm/index'
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import faqsData from '../fixtures/faqs.json';
 import { Accordion } from '../components';
+import { OptForm } from '../components';
+
 
 export function FaqsContainer() {
     return(
@@ -13,6 +15,13 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             )}
+
+            <OptForm>
+                <OptForm.Input placeholder="Email address"></OptForm.Input>
+                <OptForm.Button placeholder="Email address">Try It Now</OptForm.Button>
+                <OptForm.Text placeholder="Email address">Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
+            <OptForm.Break />
         </Accordion>
     )
 };
