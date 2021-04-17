d0287b2e42cee9722b6ab890505500599d4da89a
Andreia <andreia.ca22@gmail.com>
2021-04-17 23:47:54 +0100

    netflix - browseContainer-pt1

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useContent } from '../hooks/index';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
 
 function Browse() {
     const { series } = useContent('series');
-    console.log(series);
+    const { films } = useContent('films');
+
+    const slides = selectionFilter({ series, films });
+    console.log(slides);
 
 
-    return (
-        <p>Hello</p>
-    )
+    return <BrowseContainer slides={slides}/>
 }
 
 export default Browse;
